e0489e48ea96b6fed8d9d07d9d2ca975fcb0bcd9
Kushini <kushinirita@gmail.com>
2025-02-15 21:47:23 -0500

    Testing 123

---
+++ Resume_Critic.py
@@ -0,0 +1,31 @@
+from pypdf import PdfReader
+# import pdfplumber
+
+# path = r"C:\Users\kushi\OneDrive\Documents\University\11. Winter 2025\Coop\Kushini Lowe - Resume (Jan 14).pdf"
+
+path = r"path\to\resume"
+reader = PdfReader(path)
+
+page = reader.pages[0]
+text = page.extract_text()
+print(text)
+
+
+
+
+
+
+# def extract_text_from_pdf(pdf_path):
+#     with pdfplumber.open(pdf_path) as pdf:
+#         text = "\n".join(page.extract_text() for page in pdf.pages if page.extract_text())
+#     return text
+
+# resume_text = extract_text_from_pdf(path)
+# print(resume_text)
+
+
+
+
+
+
+
