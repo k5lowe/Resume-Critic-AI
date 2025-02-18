1f25f7fe0b810e82a8336c3ab6d801741d43a6af
Kushini <kushinirita@gmail.com>
2025-02-18 18:19:37 -0500

    Final Version

--- frontend.py
+++ frontend.py
@@ -1,34 +1,24 @@
 import streamlit as st
 from backend import main_function
-import os
 import tempfile
 
 st.title("Software Resume AI Critic")
 
 resume = st.file_uploader("Upload your resume", type=["pdf"])
-
 job_description = st.text_area("Enter the job description")
 
 
 if st.button("Analyze") and resume is not None and job_description:
     st.success("Resume uploaded successfully")
 
-    # file_path = os.path.join("temp", resume.name)
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
         
-        tmp_file.write(resume.getbuffer())  # Write uploaded content to the temp file
+        tmp_file.write(resume.getbuffer())  
         tmp_file_path = tmp_file.name
-
-
         score, feedback = main_function(tmp_file_path, job_description)
-
         st.markdown("<h2><b>Resume Score:</b></h2>", unsafe_allow_html=True)
-        
-
-
         color = f"rgb({255 - int(score * 2.55)}, {int(score * 2.55)}, 0)"
 
-        # Custom HTML to create a circle with bold text and color
         circle_html = f"""
             <div style="display: flex; justify-content: center; align-items: center; 
                         width: 100%;">
@@ -41,10 +31,7 @@ if st.button("Analyze") and resume is not None and job_description:
             </div>
         """
 
-        # Display the custom HTML in Streamlit
         st.markdown(circle_html, unsafe_allow_html=True)
-
-
         st.markdown(f"<h2><b>Feedback:</b></h2>\n {feedback}", unsafe_allow_html=True)
 
 
